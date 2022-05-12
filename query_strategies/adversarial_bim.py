92788d8b2336cc799e8e2b44a80d8c585b63733e
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-05-11 22:53:06 -0400

    DeepFool + BIM

--- query_strategies/adversarial_bim.py
+++ query_strategies/adversarial_bim.py
@@ -1,47 +1,28 @@
 import numpy as np
-import torch
-import torch.nn.functional as F
-from .strategy import Strategy
-from tqdm import tqdm
+# import torch
+from cleverhans.torch.attacks.fast_gradient_method import fast_gradient_method        
+from cleverhans.torch.attacks.projected_gradient_descent import projected_gradient_descent
 
-class AdversarialBIM(Strategy):
-    def __init__(self, dataset, net, eps=0.05):
-        super().__init__(dataset, net)
-        self.eps = eps
+from .adversarial_strategy import AdversarialStrategy
 
-    def cal_dis(self, x):
-        nx = torch.unsqueeze(x, 0)
-        nx.requires_grad_()
-        eta = torch.zeros(nx.shape)
+class AdversarialBIM(AdversarialStrategy):
+    def __init__(self, dataset, net,
+                    repeat= 1,
+                    pseudo_labeling=True,
+                    max_iter=10,
+                    n_subset_ul=None,
+                    diversity=False,
+                    dist_file_name=None, **kwargs):
+        super().__init__(dataset, net,
+                        repeat,
+                        pseudo_labeling,
+                        max_iter,
+                        n_subset_ul,
+                        diversity, dist_file_name, **kwargs)
 
-        out, e1 = self.net.clf(nx+eta)
-        py = out.max(1)[1]
-        ny = out.max(1)[1]
-        while py.item() == ny.item():
-            loss = F.cross_entropy(out, ny)
-            loss.backward()
 
-            eta = self.eps * torch.sign(nx.grad.data)            
-            nx.grad.data.zero_()
-            
-            eta = torch.clamp(eta, np.inf, self.eps)
-            eta = nx - torch.unsqueeze(x, 1)
-            out, e1 = self.net.clf(nx+eta)
-            py = out.max(1)[1]
-
-        return (eta*eta).sum()
-
-    def query(self, n):
-        unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
-
-        self.net.clf.cpu()
-        self.net.clf.eval()
-        dis = np.zeros(unlabeled_idxs.shape)
-
-        for i in tqdm(range(len(unlabeled_idxs)), ncols=100):
-            x, y, idx = unlabeled_data[i]
-            dis[i] = self.cal_dis(x)
-
-        self.net.clf.cuda()
-
-        return unlabeled_idxs[dis.argsort()[:n]]
+    def attack_fn(self, X):
+        """BIM attack 
+           cleverhans implementation of BIM (pgd with rand_init=False)
+        """
+        return projected_gradient_descent(self.net.clf, X, **self.params)
