807f7df3a56e1f811f1b1751365983e743ecf8d7
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-05-17 06:06:18 -0400

    adv debug

--- handlers.py
+++ handlers.py
@@ -10,7 +10,7 @@ class MNIST_Handler(Dataset):
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
-        self.transform = transforms.Compose([transforms.ToTensor()])#, transforms.Normalize((0.1307,), (0.3081,))])
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
     def __getitem__(self, index):
         x, y = self.X[index], self.Y[index]
