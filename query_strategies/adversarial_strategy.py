1dc2a789fc13d84bbd67267b74ce7cabbf95fcb8
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-05-31 02:54:41 -0400

    id exp in dist

--- query_strategies/adversarial_strategy.py
+++ query_strategies/adversarial_strategy.py
@@ -20,6 +20,7 @@ class AdversarialStrategy(Strategy):
         self.n_subset_ul = n_subset_ul # number of unlabeled data to attack
         self.params = kwargs
         self.dist_file_name = dist_file_name
+        self.id_exp = id_exp
 
     def check_querying(self, n_query):
         if self.n_subset_ul < n_query:
@@ -50,7 +51,7 @@ class AdversarialStrategy(Strategy):
         for i in tqdm(range(len(unlabeled_idxs)), ncols=100):
             x, y, _ = iter_loader.next()
             dis, x_adv = self.cal_dis(x)
-            log_to_file(self.dist_file_name, f'{id_exp}, {i}, {dis.numpy()}')
+            log_to_file(self.dist_file_name, f'{self.id_exp}, {i}, {dis.numpy()}')
             distances[i] = dis
             adv_images.append(x_adv.squeeze(0) if x.shape[0]==1 else x_adv)
             # k = 0 if len(adv_images)==1 else -2
