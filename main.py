1ee52574f664439a8e3be54308a407cc776536b1
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-06-14 01:52:17 -0400

    deterministic -r

--- main.py
+++ main.py
@@ -163,8 +163,8 @@ if __name__ == "__main__":
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    # torch.cuda.manual_seed(seed)
+    # torch.cuda.manual_seed_all(seed)
     # torch.backends.cudnn.benchmark = False
     # torch.backends.cudnn.deterministic = True
     # torch.backends.cudnn.enabled = False
