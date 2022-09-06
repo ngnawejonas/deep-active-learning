72a8bcd9b9cbcf2637946251d07480f45f43c849
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-09-05 23:20:40 -0400

    mnist handler

--- src/handlers.py
+++ src/handlers.py
@@ -7,10 +7,10 @@ from PIL import Image
 from typing import Tuple, Any
 
 class MNIST_Handler(Dataset):
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, train=True):
         self.X = X
         self.Y = Y
-        # self.transform = transforms.Compose([transforms.ToTensor()])#, transforms.Normalize((0.1307,), (0.3081,))])
+        # self.transform= transforms.Compose([transforms.ToTensor()])#, transforms.Normalize((0.1307,), (0.3081,))])
 
     def __getitem__(self, index):
         x, y = self.X[index], self.Y[index]
