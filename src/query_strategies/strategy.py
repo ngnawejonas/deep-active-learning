6177a24bcba1dceb793858b7fdb32f6c75997773
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-12-24 17:33:07 -0500

    cumul dis to numpy fix

--- src/query_strategies/strategy.py
+++ src/query_strategies/strategy.py
@@ -80,6 +80,8 @@ class Strategy:
             dis_inf = np.inf
             dis_2 = np.inf  
         dis = {'2': dis_2, 'inf': dis_inf}
+        if torch.is_tensor(cumul_dis):
+            cumul_dis = cumul_dis.detach().numpy()
         return nb_iter, dis, cumul_dis
 
     def eval_test_dis(self):
